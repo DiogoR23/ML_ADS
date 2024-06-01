2a9dfc3512c356e50c166e463cf9c5177f6df108
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2024-06-01 18:26:05 +0100

    commit ML_Analysis, AppProjetct, EducationEncoder commit JobEncoder, MaritalEncoder, Scaler, Selector

--- ML Project/AppProjetct.py
+++ ML Project/AppProjetct.py
@@ -1,10 +1,2 @@
 import streamlit as st
 
-st.title("Minha Primeira Aplicação com Streamlit")
-st.write("Olá, mundo! Esta é a minha aplicação web criada com Streamlit.")
-
-nome = st.text_input("Digite seu nome:")
-st.write(f"Olá, {nome}!")
-
-if st.button("Clique aqui"):
-    st.write("Você clicou no botão!")
