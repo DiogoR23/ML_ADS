3e35cf53bda7eb1b12437a4551c3352d33a9aeed
Diogo Rodrigues <diogo.gorodrigues@gmail.com>
2024-06-02 01:50:26 +0100

    commit

--- ML Project/StreamLit.py
+++ ML Project/StreamLit.py
@@ -13,15 +13,6 @@ with open('Scaler.pkl', 'rb') as scaler_file:
 with open('Selector.pkl', 'rb') as selector_file:
     selector = pickle.load(selector_file)
 
-with open('EducationEncoder.pkl', 'rb') as educationEncoder_file:
-    educationEncoder = pickle.load(educationEncoder_file)
-
-with open('JobEncoder.pkl', 'rb') as jobEncoder_file:
-    jobEncoder = pickle.load(jobEncoder_file)
-
-with open('MaritalEncoder.pkl', 'rb') as maritalEncoder_file:
-    maritalEncoder = pickle.load(maritalEncoder_file)
-
 # Options for categorical features
 job_types = ['admin.', 'blue-collar', 'entrepreneur', 'housemaid', 'management', 
              'retired', 'self-employed', 'services', 'student', 'technician', 'unemployed', 'unknown']
@@ -30,7 +21,7 @@ marital_status = ['divorced', 'married', 'single']
 
 education_levels = ['primary', 'secondary', 'tertiary', 'unknown']
 
-# Defining the streamLit interface
+# Defining the Streamlit interface
 st.title("Forecast using Random Forest")
 
 # Input by the User
@@ -49,12 +40,12 @@ campaign = st.number_input("Number of times contacted in this campaign", min_val
 pdays = st.number_input("Number of days since the customer was last contacted", min_value=-1)
 previous = st.number_input("Number of times contacted before this campaign", min_value=0)
 
-# Converting Yes & No values for numeric
+# Converting Yes & No values to numeric
 default = 1 if default == 'Yes' else 0
 housing = 1 if housing == 'Yes' else 0
 loan = 1 if loan == 'Yes' else 0
 
-# Converting data into Dataframe
+# Converting data into DataFrame
 data = {
     'age': [age],
     'job': [job],
@@ -74,12 +65,26 @@ data = {
 bank = pd.DataFrame(data)
 
 # Functions for pre-processing input data
-def encode_variables(df):
-    df['job'] = jobEncoder.transform(df[['job']])
-    df['marital'] = maritalEncoder.transform(df[['marital']])
-    df['education'] = educationEncoder.transform(df[['education']])
+def oneHotEncoder_job(df, job_var):
+    for job_type in job_types:
+        if job_var == job_type:
+            df[f'job_{job_type}'] = 1
+        else:
+            df[f'job_{job_type}'] = 0
     return df
 
+def oneHotEncoder_marital(df, marital_var):
+    for marital_stat in marital_status:
+        if marital_var == marital_stat:
+            df[f'marital_{marital_stat}'] = 1
+        else:
+            df[f'marital_{marital_stat}']= 0
+    return df
+
+def labelEncoder_education(df):
+    education_map = {'primary': 0, 'secondary': 1,'tertiary': 2, 'unknown': 3}
+    df['education'] = df['education'].map(education_map)
+    return df
 
 def cyclical_encoding(df):
     num_month = {'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'may': 5, 'jun': 6, 'jul': 7, 'aug': 8, 'sep': 9, 'oct': 10, 'nov': 11, 'dec': 12}
@@ -91,30 +96,45 @@ def cyclical_encoding(df):
     return df
 
 def log_transform(df):
-    df['balance'] = np.log(df['balance'] + 1)
+    df['balance_log'] = np.log(df['balance'] + 1)
+    df.drop('balance', axis=1, inplace=True)
     return df
 
-def scale_data(df):
-    num_vars = df.select_dtypes(include=[np.number])
+def scale_data(df, num_var):
+    num_vars = df[num_var]
     scaled_data = scaler.transform(num_vars)
-    df[num_vars.columns] = scaled_data
+    df[num_var] = scaled_data
     return df
 
 def select_features(df):
     selected_features = selector.transform(df)
     return selected_features
 
-# Apply pre-processing to input data
-bank = encode_variables(bank)
-bank = cyclical_encoding(bank)
-bank = log_transform(bank)
-bank = scale_data(bank)
+# Make the predictor button
+if st.button('Make Predictions'):
+    # Apply pre-processing to input data
+    bank = cyclical_encoding(bank)
+    bank = oneHotEncoder_job(bank, job)
+    bank = oneHotEncoder_marital(bank, marital)
+    bank = labelEncoder_education(bank)
+    bank = log_transform(bank)
+    num_var = ['age', 'balance_log', 'education', 'day', 'month_sin', 'month_cos', 'campaign', 'pdays', 'previous']
+    bank = scale_data(bank, num_var)
 
-# Select the best features
-bank_selected = select_features(bank)
+    # Ensure the DataFrame has all the expected columns
+    expected_columns = selector.get_feature_names_out()
+    for col in expected_columns:
+        if col not in bank.columns:
+            bank[col] = 0
+    
+    # Reorder the DataFrame to match the expected columns
+    bank = bank[expected_columns]
+    
+    # Select the best features
+    bank_selected = select_features(bank)
 
-# Making predictions with created model
-prediction = model.predict(bank_selected)
+    # Making predictions with created model
+    prediction = model.predict(bank_selected)
 
-# Show prediction
-st.write("Prediction:", "Yes" if prediction[0] == 1 else "No")
+    # Show prediction
+    st.write("Prediction:", "Yes" if prediction[0] == 1 else "No")
